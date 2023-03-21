bab3026c2370b00ed70706a84a334891c2209983
mamsdeveloper <belk1naalyona@yandex.ru>
2023-03-21 07:13:21 +0300

    add users mode in accounts menu

--- BotMicro/bot/handlers/superuser/accounts_list.py
+++ BotMicro/bot/handlers/superuser/accounts_list.py
@@ -19,7 +19,7 @@ async def accounts_list_handler(query: CallbackQuery, state: FSMContext):
     if not message:
         return
 
-    users = User.query(User.mode.not_contains('superuser')) # type: ignore
+    users = User.query(User.mode.not_contains('superuser'))  # type: ignore
     users_list_kb = build_users_list_kb(users)
     await message.edit_text(
         text=messages.ACCOUNTS_LIST,
@@ -28,16 +28,21 @@ async def accounts_list_handler(query: CallbackQuery, state: FSMContext):
 
 
 def build_users_list_kb(users: list[User]) -> InlineKeyboardMarkup:
+    modes_table = {
+        'admin': 'Администратор',
+        'employee': 'Сотрудник',
+        'superuser': 'Владелец',
+    }
     kb = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='Назад', callback_data=AccountsCallback().pack())]
-    ])
-    kb.inline_keyboard += [
         [
             InlineKeyboardButton(
-                text=user.name, 
-                callback_data=UserInfoCallback(key=user.key).pack() # type: ignore
+                text=f'{user.name} - {modes_table[user.mode]}',
+                callback_data=UserInfoCallback(key=user.key).pack()  # type: ignore
             )
         ] for user in users
-    ]
+    ])
+    kb.inline_keyboard.append([
+        InlineKeyboardButton(text='Назад', callback_data=AccountsCallback().pack())
+    ])
 
     return kb
