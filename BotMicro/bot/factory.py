6723de6af2ae06392b8577bd781960a2e89bca14
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-05 19:44:17 +0300

    refactor routers connections | connect middlewares

--- BotMicro/bot/factory.py
+++ BotMicro/bot/factory.py
@@ -2,14 +2,25 @@ from aiogram import Bot, Dispatcher
 from aiogram_deta.storage import DetaStorage
 from deta import Deta  # type: ignore
 
-from bot import routing
+from bot.handlers import admin, common, employee, superuser
+from bot.middlewares.callback_answer import CallbackAnswerMiddleware
+from bot.middlewares.callback_message import CallbackMessageMiddleware
+from bot.middlewares.logging import LoggingMiddleware
 
 
 def create_dispatcher(deta: Deta) -> Dispatcher:
     storage = DetaStorage(deta_base=deta.AsyncBase('fsm'))
     dispatcher = Dispatcher(storage=storage)
 
-    routing.register_handlers(dispatcher)
+    dispatcher.include_router(admin.router)
+    dispatcher.include_router(employee.router)
+    dispatcher.include_router(superuser.router)
+    dispatcher.include_router(common.router)
+    
+    dispatcher.callback_query.middleware(CallbackAnswerMiddleware())
+    dispatcher.callback_query.middleware(CallbackMessageMiddleware())
+    dispatcher.callback_query.middleware(LoggingMiddleware())
+    dispatcher.message.middleware(LoggingMiddleware())
 
     return dispatcher
 
--- BotMicro/bot/handlers/common/__init__.py
+++ BotMicro/bot/handlers/common/__init__.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 
-from . import start, login, backdoor, logout, error
+from . import start, login, backdoor, logout, error, other
 
 router = Router()
 router.include_router(start.router)
@@ -8,3 +8,4 @@ router.include_router(login.router)
 router.include_router(logout.router)
 router.include_router(backdoor.router)
 router.include_router(error.router)
+# router.include_router(other.router)
